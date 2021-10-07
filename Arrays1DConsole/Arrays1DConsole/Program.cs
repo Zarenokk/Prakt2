2a8552ca6d982ee8d139c10046cabcce169c799f
Zarenokk <90775928+Zarenokk@users.noreply.github.com>
2021-10-07 21:00:34 +0300

    Add files via upload

---
+++ Arrays1DConsole/Arrays1DConsole/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace Arrays1DConsole
+{
+    class Program
+    {//14 Варіант
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            ushort n;
+            ushort k = 0;
+            double S=0;
+            bool f,kf;
+            do
+            {
+                Console.Write("Введіть кількість елементів масиву(ціле число): ");
+                f = ushort.TryParse(Console.ReadLine(),out n);
+                if (f == false) {
+                    Console.WriteLine("Помилка! Введіть ще раз");
+                }
+                if (n == 0) {
+                    n = 10;
+                }
+            } while (!f);
+            Console.WriteLine(n);
+            Console.WriteLine(" ");
+            double[] arr = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                double d = rnd.Next(-15, 19);
+                double d2 = rnd.NextDouble();
+                double d3 = d + d2;
+                double dx = Math.Round(d3, 1);
+                arr[i] = dx;
+                Console.WriteLine(arr[i]);
+            }
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > 0) {
+                    S = S + arr[i];
+                }
+            }
+            Console.Write("Сума додатніх індексів = ");
+            Console.WriteLine(S);
+            Console.WriteLine("");
+            //СОРТУВАННЯ
+            do
+            {
+                Console.Write("Введіть кількість елементів які будуть сортуватись(не більше розміру масиву): ");
+                kf = ushort.TryParse(Console.ReadLine(), out k);
+                if (k > n) {
+                    kf = false;
+                }
+                if (kf == false)
+                {
+                    Console.WriteLine("Помилка! Введіть ще раз");
+                }
+            } while (!kf);
+            double temp;
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = i + 1; j <k; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вивід відсортованого масиву");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Arrays1DConsole/Arrays2DConsole/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+
+namespace Arrays2DConsole
+{
+    class Program
+    {//8 Варіант
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            ushort n;//рядки матриці
+            ushort m;//стовпці матриці
+            bool fn,fm;
+            //ввід кількості рядків
+            do
+            {
+                Console.Write("Введіть кількість рядків матриці(ціле число): ");
+                fn = ushort.TryParse(Console.ReadLine(), out n);
+                if (fn == false)
+                {
+                    Console.WriteLine("Помилка! Введіть ще раз");
+                }
+                if (n == 0)
+                {
+                    n = 4;
+                }
+            } while (!fn);
+            //ввід кількості стовпців
+            do
+            {
+                Console.Write("Введіть кількість стовпців матриці(ціле число): ");
+                fm = ushort.TryParse(Console.ReadLine(), out m);
+                if (fm == false)
+                {
+                    Console.WriteLine("Помилка! Введіть ще раз");
+                }
+                if (n == 0)
+                {
+                    n = 4;
+                }
+            } while (!fm);
+            double[,] matr = new double[n, m];
+            //заповнення матриці рандомними числами від[-33.531; 33.110]
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++) {
+                    double d = rnd.Next(-34,34);
+                    double d2 = rnd.NextDouble();
+                    double d3 = d + d2;
+                    double dx = Math.Round(d3, 3);
+                    matr[i,j] = dx;
+                }
+            }
+            //Виведення матриці
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matr[i, j]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Виведення суми модулів кожного рядка:");
+            //Виведення суми модулів кожного рядка
+            double[] arrS = new double[n];
+            double Suma = 0;
+            double naySuma = 0;
+            int nomerR = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Suma = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    Suma=Suma+Math.Abs(matr[i, j]);
+                }
+                arrS[i] = Suma;
+                Console.WriteLine(Suma);
+                Console.WriteLine(" ");
+            }
+            //Виведення найбільшої суми модулів рядка
+            Console.WriteLine("Виведення найбільшої суми модулів рядка:");
+            for (int i = 0; i < n; i++) {
+                if (naySuma < arrS[i]) {
+                    naySuma = arrS[i];
+                    nomerR = i;
+                }
+            }
+            Console.WriteLine($"{++nomerR} номер рядка,значення = {naySuma}");
+        }
+    }
+}
